7caafb0db6de42024bbed2af22a203853ecd7184
Mr.Kangs <gykang00@gmail.com>
2023-05-21 19:29:06 -0700

    init website format

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('organizationButton').addEventListener('click', function() {
+    window.open('https://github.com/ChatImproVR', '_blank');
+  });
+
+document.getElementById('documentationButton').addEventListener('click', function() {
+    window.open('https://chatimprovr.github.io/The-Book/', '_blank');
+  });
+
+document.getElementById('sourceCodeButton').addEventListener('click', function() {
+    window.open('https://github.com/ChatImproVR/chatimprovr', '_blank');
+  });
+
+  
