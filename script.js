2749f0b469b431ad5e1142ada5289947a3d6d8a4
Grace Todd <56061990+toddgr@users.noreply.github.com>
2023-05-24 14:20:45 -0700

    Add image gallery. Gonna need to add cooler images folks

--- script.js
+++ script.js
@@ -10,4 +10,33 @@ document.getElementById('sourceCodeButton').addEventListener('click', function()
     window.open('https://github.com/ChatImproVR/chatimprovr', '_blank');
   });
 
+document.addEventListener("DOMContentLoaded", function() {
+  var carouselIndex = 0;
+  var carouselImages = document.querySelectorAll(".carousel img");
+  var carouselPrev = document.querySelector(".carousel-prev");
+  var carouselNext = document.querySelector(".carousel-next");
+
+  function showSlide(index) {
+    carouselImages.forEach(function(image) {
+      image.classList.remove("active");
+    });
+    carouselImages[index].classList.add("active");
+  }
+
+  function prevSlide() {
+    carouselIndex = (carouselIndex - 1 + carouselImages.length) % carouselImages.length;
+    showSlide(carouselIndex);
+  }
+
+  function nextSlide() {
+    carouselIndex = (carouselIndex + 1) % carouselImages.length;
+    showSlide(carouselIndex);
+  }
+
+  carouselPrev.addEventListener("click", prevSlide);
+  carouselNext.addEventListener("click", nextSlide);
+
+  // Show the first image
+  showSlide(carouselIndex);
+});
   
